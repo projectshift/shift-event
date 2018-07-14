d35afb6459bf450d18ebc4ebc03f3a174a10da4e
Dmitry Belyakov <dmitrybelyakov@gmail.com>
2018-07-14 10:44:26 +0100

    Updating shiftschema requirement

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from setuptools import setup, find_packages
 # ----------------------------------------------------------------------------
 
 # project version
-version = '0.0.3'
+version = '0.0.4'
 
 # development status
 # dev_status = '1 - Planning'
@@ -88,7 +88,7 @@ setup(**dict(
 
     # project dependencies
     install_requires=[
-        'shiftschema==0.1.4',
+        'shiftschema>=0.2.0,<0.3.0',
         'SQLAlchemy==1.2.7'
     ],
 
